be47ca45ee02dd79d79e1ab8eefa7a1b1cf2509b
Strabatras <strabatras@gmail.com>
2024-02-10 16:21:49 +0300

    HW-10 : структура модуля

---
+++ hw10-aop/build.gradle.kts
@@ -0,0 +1,37 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id ("com.github.johnrengelman.shadow")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+}
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("hw10-aop")
+        archiveVersion.set("0.1")
+        archiveClassifier.set("")
+        manifest {
+            attributes(mapOf("Main-Class" to "ru.otus.Main"))
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
