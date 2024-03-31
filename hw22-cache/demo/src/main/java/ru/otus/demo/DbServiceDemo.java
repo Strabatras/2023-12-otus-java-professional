55f42525337b06ad3f4f39a2bde72ca05398c5f9
Strabatras <strabatras@gmail.com>
2024-03-31 19:35:01 +0300

    изменено место расположения  файлов шаблона кэша

--- hw22-cache/demo/src/main/java/ru/otus/demo/DbServiceDemo.java
+++ hw22-cache/demo/src/main/java/ru/otus/demo/DbServiceDemo.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.MyCache;
 import ru.otus.core.repository.executor.DbExecutorImpl;
 import ru.otus.core.sessionmanager.TransactionRunnerJdbc;
 import ru.otus.crm.datasource.DriverManagerDataSource;
